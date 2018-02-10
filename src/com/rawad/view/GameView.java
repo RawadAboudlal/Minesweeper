183ebcfc1ba60f7d34025dc6a951858ec1f1869e
Rawad Aboudlal <Rawadaboudlal@yahoo.com>
2018-02-09 21:16:32 -0500

    Added TileView.
    - Adding basic view possibilities such as rendering a tile.
    - Setting up way for having different screens and switching ebtwen using
    CardLayout.

--- src/com/rawad/view/GameView.java
+++ src/com/rawad/view/GameView.java
@@ -1,6 +1,7 @@
 package com.rawad.view;
 
 import java.awt.CardLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -13,6 +14,9 @@ import com.rawad.model.GameModel;
  */
 public class GameView {
 
+  private static final String MENU = "menu";
+  private static final String GAME = "game";
+
   private static final int DEFAULT_WIDTH = 640;
   private static final int DEFAULT_HEIGHT = 480;
 
@@ -22,6 +26,8 @@ public class GameView {
 
   private JPanel menuPanel;
   private JPanel gamePanel;
+
+  private CardLayout cardLayout;
 
   private GameModel gameModel;
 
@@ -51,10 +57,17 @@ public class GameView {
     frame = new JFrame("");
 
     basePanel = new JPanel();
+    menuPanel = new JPanel();
+    gamePanel = new JPanel();
 
-    CardLayout cardLayout = new CardLayout();
+    cardLayout = new CardLayout();
 
     basePanel.setLayout(cardLayout);
+
+    basePanel.add(menuPanel, MENU);
+    basePanel.add(gamePanel, GAME);
+
+    basePanel.setPreferredSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
 
     frame.setVisible(true);
 
---
+++ src/com/rawad/view/TileView.java
@@ -0,0 +1,96 @@
+package com.rawad.view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JButton;
+import com.rawad.model.Tile;
+
+/**
+ * @author Rawad
+ *
+ */
+public class TileView extends JButton {
+
+  private static final int DEFAULT_WIDTH = 16;
+  private static final int DEFAULT_HEIGHT = 16;
+
+  private static final int ROUNDNESS = 5;
+
+  /**
+   * Generated serial version UID.
+   */
+  private static final long serialVersionUID = 3353075477507007301L;
+
+  private Tile tile;
+
+  public TileView(Tile tile) {
+    super();
+    this.tile = tile;
+
+    this.setPreferredSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
+
+  }
+
+  /**
+   * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+   */
+  @Override
+  protected void paintComponent(Graphics g) {
+
+    switch (tile.getState()) {
+      case COVERED:
+        g.setColor(Color.GRAY);
+        g.fillRoundRect(0, 0, this.getWidth(), this.getHeight(), ROUNDNESS, ROUNDNESS);
+      case FLAGGED:
+        g.setColor(Color.RED);
+        g.drawOval(0, 0, this.getWidth(), this.getHeight());
+        break;
+      case OPENED:
+        this.paintOpened(g);
+    }
+
+  }
+
+  private void paintOpened(Graphics g) {
+
+    String s = "";
+
+    switch (tile.getContent()) {
+      case NONE:
+        break;
+      case ONE:
+        s = "1";
+        break;
+      case TWO:
+        s = "2";
+        break;
+      case THREE:
+        s = "3";
+        break;
+      case FOUR:
+        s = "4";
+        break;
+      case FIVE:
+        s = "5";
+        break;
+      case SIX:
+        s = "6";
+        break;
+      case SEVEN:
+        s = "7";
+        break;
+      case EIGHT:
+        s = "8";
+      case BOMB:
+        g.setColor(Color.BLACK);
+        g.fillOval(0, 0, this.getWidth(), this.getHeight());
+        return;
+    }
+
+    g.setColor(Color.BLACK);
+    g.drawString(s, 0, this.getHeight());
+
+  }
+
+}

b9c254e4371018e03dbabed19ab152e9f9073aa9
Rawad Aboudlal <Rawadaboudlal@yahoo.com>
2018-02-10 16:00:34 -0500

    Moved Difficulty to GameModel.
    - Added GameController.generateEmptyBoard() for making the initial board
    that the player starts with.

--- src/com/rawad/controller/GameController.java
+++ src/com/rawad/controller/GameController.java
@@ -2,6 +2,8 @@ package com.rawad.controller;
 
 import com.rawad.model.GameModel;
 import com.rawad.model.Tile;
+import com.rawad.model.TileContent;
+import com.rawad.model.TileState;
 import com.rawad.utils.Difficulty;
 
 /**
@@ -12,13 +14,13 @@ public class GameController {
 
   private GameModel model;
 
-  private Difficulty difficulty;
-
   public void initializeGame(Difficulty difficulty) {
 
-    this.difficulty = difficulty;
+    model = new GameModel();
 
-    this.model = new GameModel();
+    model.setDifficulty(difficulty);
+
+    model.setBoard(GameController.generateEmptyBoard(difficulty));
 
   }
 
@@ -26,18 +28,31 @@ public class GameController {
 
   }
 
-  /**
-   * {@code startingTileX}, {@code startingTileY} should not be a bomb.
-   * 
-   * @param difficulty
-   * @param startingTileX
-   * @param startingTileY
-   * @return
-   */
-  public static Tile[][] generateBoard(Difficulty difficulty, int startingTileX,
-      int startingTileY) {
+  public static Tile[][] generateEmptyBoard(Difficulty difficulty) {
 
-    Tile[][] board = new Tile[difficulty.getWidth()][difficulty.getHeight()];
+    Tile[][] board = new Tile[difficulty.getHeight()][difficulty.getWidth()];
+
+    for (int y = 0; y < board.length; y++) {
+      for (int x = 0; x < board[y].length; x++) {
+
+        Tile tile = new Tile();
+
+        tile.setX(x);
+        tile.setY(y);
+
+        tile.setState(TileState.COVERED);
+        tile.setContent(TileContent.NONE);
+
+        board[y][x] = tile;
+
+      }
+    }
+
+    return board;
+
+  }
+
+  public static Tile[][] generateBoard(Tile[][] board, Difficulty difficulty) {
 
     for (int y = 0; y < board.length; y++) {
       for (int x = 0; x < board[y].length; x++) {
--- src/com/rawad/model/GameModel.java
+++ src/com/rawad/model/GameModel.java
@@ -1,5 +1,6 @@
 package com.rawad.model;
 
+import com.rawad.utils.Difficulty;
 
 /**
  * @author Rawad
@@ -9,6 +10,34 @@ public class GameModel {
 
   private Tile[][] board;
 
+  private Difficulty difficulty;
 
+  /**
+   * @return the board
+   */
+  public Tile[][] getBoard() {
+    return board;
+  }
+
+  /**
+   * @param board the board to set
+   */
+  public void setBoard(Tile[][] board) {
+    this.board = board;
+  }
+
+  /**
+   * @return the difficulty
+   */
+  public Difficulty getDifficulty() {
+    return difficulty;
+  }
+
+  /**
+   * @param difficulty the difficulty to set
+   */
+  public void setDifficulty(Difficulty difficulty) {
+    this.difficulty = difficulty;
+  }
 
 }
